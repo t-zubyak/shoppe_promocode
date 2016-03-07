857d8bbf94e76096e38a15cf660cd7816dbead98
André Tanguy <andre.tanguy@oceanshq.com>
2016-03-07 15:43:35 +0000

    update navigation

--- lib/shoppe_promocode.rb
+++ lib/shoppe_promocode.rb
@@ -2,5 +2,19 @@ require "shoppe_promocode/engine"
 require "shoppe/navigation_manager"
 
 module ShoppePromocode
-  #TODO: prepend a navigational item for Promo Codes 
+
+  #TODO: only prepend a navigational item for Promo Codes rather than re-writing the entire navigation
+  Shoppe::NavigationManager.build(:admin_primary) do
+    add_item :customers
+    add_item :orders
+    add_item :products
+    add_item :product_categories
+    add_item :delivery_services
+    add_item :tax_rates
+    add_item :promocodes
+    add_item :users
+    add_item :countries
+    add_item :settings
+  end
+
 end
--- lib/shoppe_promocode/version.rb
+++ lib/shoppe_promocode/version.rb
@@ -1,3 +1,3 @@
 module ShoppePromocode
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
